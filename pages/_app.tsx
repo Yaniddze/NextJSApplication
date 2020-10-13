cc64658afe44fffafa48406ba931c1de9ee4134d
Arcusterrr <nikita.kargapolov@gmail.com>
2020-10-13 19:29:07 +0500

    :wrench: Add default styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import { FC, ReactNode } from 'react';
+import { createGlobalStyle } from 'styled-components';
 
 type PropTypes = {
   Component: FC,
   pageProps: any,
 }
 
+const Styles = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
 export default function MyApp(
   { Component, pageProps }: PropTypes,
 ): ReactNode {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Styles />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,9 +7,6 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>
-            Profile
-          </title>
           <link
             href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
             rel="stylesheet"
