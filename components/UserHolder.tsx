ee85af426e8277375f456161a7f0771e3af05a98
Zinevich Yan <facecrack1337@gmail.com>
2020-10-14 13:42:00 +0500

    :art: Add adaptive to user holder

--- components/UserHolder.tsx
+++ components/UserHolder.tsx
@@ -1,4 +1,4 @@
-import { FC, useState, MouseEvent } from 'react';
+import { FC, MouseEvent } from 'react';
 import styled from 'styled-components';
 
 import CreateIcon from '@material-ui/icons/Create';
@@ -6,9 +6,17 @@ import CloseIcon from '@material-ui/icons/Close';
 
 import { AccountIcon } from './AccountIcon';
 
-const Wrapper = styled.div`
+import { MinWidths } from '../app/screens';
+import { useScreens } from '../hooks/useScreens';
+
+type WidthProps = {
+  mobile: boolean;
+}
+
+const Wrapper = styled.div<WidthProps>`
+  height: ${(props): string => (props.mobile ? '70' : '130')}px;
+
   border-radius: 10px;
-  height: 130px;
   background: #1A78C2;
   display: flex;
   align-items: center;
@@ -24,27 +32,56 @@ const Wrapper = styled.div`
   }
 `;
 
-const LeftWrapper = styled.div`
-  margin-left: 30px;
-  
-  font-size: 30px;
-  font-weight: 600;
-  
-  & > div:last-child {
-    margin-left: 40px;
-  }
+const LeftWrapper = styled.div<WidthProps>`
+  ${(props): string => {
+    if (props.mobile) {
+      return `
+        margin-left: 10px;
+        font-size: 14px;
+        
+        & > div:last-child {
+          margin-left: 10px;
+        }
+      `;
+    }  
+    
+    return `
+      font-size: 30px;
+      font-weight: 600;
+      margin-left: 30px;
+      
+      & > div:last-child {
+        margin-left: 40px;
+      }
+    `;
+  }}
 `;
 
-const RightWrapper = styled.div`
-  margin-right: 60px;
-  
+const RightWrapper = styled.div<WidthProps>`
+  ${(props): string => {
+    if (props.mobile) {
+      return `
+        margin-right: 10px;
+      `;
+    }  
+    
+    return `
+      margin-right: 60px;
+    
+      & > div:first-child {
+        margin-right: 10px;
+      }
+    `;
+  }}
+
   &:hover {
     cursor: pointer;
   }
   
-  & > div:first-child {
-    margin-right: 10px;
-  }
+`;
+
+const IconTitleWrapper = styled.div<WidthProps>`
+  display: ${(props): string => (props.mobile ? 'none' : 'block')};
 `;
 
 type PropTypes = {
@@ -63,35 +100,57 @@ export const UserHolder: FC<PropTypes> = (
     onEditChange();
   };
 
+  const screen = useScreens();
+
+  let accountSize = 0;
+
+  let mobile = false;
+
+  switch (screen) {
+    case MinWidths.Mobile:
+      accountSize = 40;
+      mobile = true;
+      break;
+
+    case MinWidths.PC:
+      accountSize = 80;
+      mobile = false;
+      break;
+
+    default:
+      // eslint-disable-next-line no-case-declarations,@typescript-eslint/no-unused-vars
+      const x: never = screen;
+  }
+
   const editButton = !editing && (
     <>
-      <div>
+      <IconTitleWrapper mobile={mobile}>
         Редактировать
-      </div>
+      </IconTitleWrapper>
       <CreateIcon />
     </>
   );
 
   const closeButton = editing && (
     <>
-      <div>
+      <IconTitleWrapper mobile={mobile}>
         Закрыть
-      </div>
+      </IconTitleWrapper>
       <CloseIcon />
     </>
   );
 
   return (
-    <Wrapper>
+    <Wrapper mobile={mobile}>
 
-      <LeftWrapper>
-        <AccountIcon width={80} height={80} />
+      <LeftWrapper mobile={mobile}>
+        <AccountIcon width={accountSize} height={accountSize} />
         <div>
           { username }
         </div>
       </LeftWrapper>
 
-      <RightWrapper onClick={handleClick}>
+      <RightWrapper mobile={mobile} onClick={handleClick}>
         {editButton}
         {closeButton}
       </RightWrapper>
