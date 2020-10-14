c1a67ba5d0c9d3df33c96ee27fd7a0ae23683760
Zinevich Yan <facecrack1337@gmail.com>
2020-10-14 13:28:22 +0500

    :tada: Create screens tools

---
+++ app/screens.ts
@@ -0,0 +1,4 @@
+export enum MinWidths {
+  Mobile = 0,
+  PC = 850,
+}
---
+++ hooks/useScreens.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import { MinWidths } from '../app/screens';
+
+export const useScreens = (): MinWidths => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = (): void => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+  
+  if (width >= MinWidths.PC) return MinWidths.PC;
+  
+  if (width >= MinWidths.Mobile) return MinWidths.Mobile;
+
+  return MinWidths.PC;
+};
